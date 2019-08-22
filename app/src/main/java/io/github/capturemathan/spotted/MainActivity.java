c053bc6a94c9e3964638ec05f93d94061fea8d80
Mathan.S <capturesmk@gmail.com>
2019-08-22 12:30:35 +0530

    Added Shimmer :blink:

--- app/src/main/java/io/github/capturemathan/spotted/MainActivity.java
+++ app/src/main/java/io/github/capturemathan/spotted/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     Button b1, b2;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ShimmerFrameLayout container = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmer();
         b1 = findViewById(R.id.detailsbtn);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
