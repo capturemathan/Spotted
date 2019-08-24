1e1208f2269c2597e86e59aecb31d091a7325c22
Mathan.S <capturesmk@gmail.com>
2019-08-24 21:41:16 +0530

    Initial PHP Setup done :>

--- app/src/main/java/io/github/capturemathan/spotted/DetailsFragment.java
+++ app/src/main/java/io/github/capturemathan/spotted/DetailsFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -15,6 +16,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -26,13 +29,23 @@ public class DetailsFragment extends Fragment {
     Button choose_photo, upload_photo;
     Bitmap bitmap;
     Uri filePath;
+    EditText name, age, phone, iden;
+    String missingname, missingage, missingphone, missingiden;
+    String url;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_details, null);
+
         choose_photo = rootview.findViewById(R.id.details_choosephotos);
         upload_photo = rootview.findViewById(R.id.details_uploadphotos);
+        name = rootview.findViewById(R.id.details_name);
+        age = rootview.findViewById(R.id.details_age);
+        phone = rootview.findViewById(R.id.details_phone);
+        iden = rootview.findViewById(R.id.details_identification);
+
+
         choose_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,12 +59,54 @@ public class DetailsFragment extends Fragment {
         upload_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                missingname = name.getText().toString();
+                missingage = age.getText().toString();
+                missingphone = phone.getText().toString();
+                missingiden = iden.getText().toString();
+                new getresponse().execute();
             }
         });
 
         return rootview;
 
+    }
+
+    private class getresponse extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getActivity(), "Sending Data", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            url = "http://192.168.43.191/missing.php?name=" + missingname + "&age=" + missingage + "&iden=" + missingiden + "&phone=" + missingphone;
+            Log.v("url", url);
+            /*HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url);
+            Log.e("MainActivity", "Response from url: " + jsonStr);
+            try {
+                Document doc = Jsoup.connect(url).get();
+                Log.v("page", doc.toString());
+                Element e = doc.select("p#mathan").first();
+                m = e.text();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }*/
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            //t.setText(m);
+            Toast.makeText(getActivity(), "Details Submitted", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+            super.onPostExecute(aVoid);
+        }
     }
 
     @Override
